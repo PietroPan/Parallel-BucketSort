226119f928f84ee94db3488b66de147169614ab5
Alexandre Gomes pg46950@alunos.uminho.pt <pg46950@search7edu.local>
2022-01-17 15:45:06 +0000

    deleted files

--- quick_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef __QUICK_SORT__
-#define __QUICK_SORT__
-
-void swap(int *a, int *b);
-int partition(int array[], int low, int high);
-void quickSort(int array[], int low, int high);
-
-#endif
