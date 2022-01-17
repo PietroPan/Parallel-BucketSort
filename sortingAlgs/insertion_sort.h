ecf7774f26513a43c12dcab850738cd917e37ad1
Alexandre Gomes pg46950@alunos.uminho.pt <pg46950@search7edu.local>
2022-01-17 15:40:06 +0000

    new optimization

---
+++ sortingAlgs/insertion_sort.h
@@ -0,0 +1,8 @@
+#ifndef __INSERTION_SORT__
+#define __INSERTION_SORT__
+
+void insertionSort(int arr[], int n);
+void printArray(int arr[], int n);
+
+#endif
+
---
+++ sortingAlgs/mergeSort.h
@@ -0,0 +1,8 @@
+#ifndef __MERGE_SORT__
+#define __MERGE_SORT__
+
+void mergeSort(int arr[],int l,int r);
+void merge(int arr[], int l,int m,int r);
+
+#endif
+
